df99fd2221309c41d91409b354dcffc010401a1b
JordLoop <Jordloop@gmail.com>
2018-01-25 15:52:28 -0800

    add read and write services for posts.service

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database-deprecated';
+import * as firebase from 'firebase';
 
 @Injectable()
 export class PostsService {
 
-  constructor() { }
+  constructor(
+    private db: AngularFireDatabase
+  ) { }
 
+  getPosts() {
+    return this.db.list('postsData/posts')
+  }
+
+  savePost(postData) {
+    const postToSave = {
+      name: 'testPost',
+      postContent: 'hello post!'
+    }
+    this.getPosts().push(postData);
+    } 
+
+    getPostByKey(postKey) {
+      return this.db.object(`postData/posts/${postKey}`);
+    }
 }
