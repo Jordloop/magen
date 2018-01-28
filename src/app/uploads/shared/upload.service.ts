18415294528eaf6549eaf7e6b821068ff5b14fc5
JordLoop <JordLoop@gmail.com>
2018-01-27 16:17:20 -0800

    add upload and delete methods for firebase storage

--- src/app/uploads/shared/upload.service.ts
+++ src/app/uploads/shared/upload.service.ts
@@ -1,8 +1,63 @@
 import { Injectable } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database-deprecated';
+import { Upload } from './upload';
+import * as firebase from 'firebase';
 
 @Injectable()
 export class UploadService {
+  constructor(private af: AngularFireModule, private db: AngularFireDatabase) { }
 
-  constructor() { }
+  private basePath: string = '/uploads';
+  private uploadTask: firebase.storage.UploadTask;
+  uploads: FirebaseListObservable<any>;
 
+  //Uploads file to firebase storage
+  pushUpload(upload: Upload) {
+    let storageRef = firebase.storage().ref();
+    this.uploadTask = storageRef.child(`${this.basePath}/${upload.file.name}`).put(upload.file);
+
+    this.uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
+      (snapshot) => {
+        // upload in progress
+        upload.progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+      },
+      (error) => {
+        // upload failed
+        console.log(error)
+      },
+      () => {
+        // upload success
+        upload.url = this.uploadTask.snapshot.downloadURL
+        upload.name = upload.file.name
+        this.saveFileData(upload)
+      }
+    );
+  }
+
+  // Writes the file details to the realtime db
+  private saveFileData(upload: Upload) {
+    this.db.list(`${this.basePath}/`).push(upload);
+  }
+
+  //Deletes file from firebase storeage
+  deleteUpload(upload: Upload) {
+    this.deleteFileData(upload.$key)
+    .then( () => {
+      this.deleteFileStorage(upload.name)
+    })
+    .catch(error => console.log('error', error))
+  }
+
+  //writes the file details to the realtime db
+  private deleteFileData(key: string) {
+    return this.db.list(`${this.basePath}/`).remove(key);
+  }
+
+  //firebase files must have unique names in their representive storage dir
+  //so the name services as a unique key
+  private deleteFileStorage(name: string) {
+    let storageRef = firebase.storage().ref();
+    storageRef.child(`${this.basePath}/${name}`).delete()
+  }
 }
