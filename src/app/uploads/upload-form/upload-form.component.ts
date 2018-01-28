053bb7887f593b7ca066cdcc5917b508aaa90a56
JordLoop <JordLoop@gmail.com>
2018-01-28 15:18:21 -0800

    add form for uploading file and progress bar

--- src/app/uploads/upload-form/upload-form.component.ts
+++ src/app/uploads/upload-form/upload-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UploadService } from '../shared/upload.service';
+import { Upload } from '../shared/upload';
+import * as _ from "lodash";
 
 @Component({
   selector: 'app-upload-form',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadFormComponent implements OnInit {
 
-  constructor() { }
+  selectedFiles: FileList;
+  currentUpload: Upload;
+
+  constructor( private uploadSvc: UploadService) { }
 
   ngOnInit() {
+  }
+
+  detectFiles(event) {
+    this.selectedFiles = event.target.files;
+  }
+
+  uploadSingle() {
+    let file = this.selectedFiles.item(0)
+    this.currentUpload = new Upload(file);
+    this.uploadSvc.pushUpload(this.currentUpload)
+  }
+
+  uploadMulti() {
+    let files = this.selectedFiles
+    let filesIndex = _.range(files.length)
+    _.each(filesIndex, (idx) => {
+      this.currentUpload = new Upload(files[idx]);
+      this.uploadSvc.pushUpload(this.currentUpload)}
+    )
   }
 
 }
