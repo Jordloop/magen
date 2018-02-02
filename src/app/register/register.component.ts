bebd8f3441162dc4d02c0b342e9fa6d5fa4d76f8
JordLoop <Jordloop@gmail.com>
2018-02-02 04:25:19 -0800

    implement system for registering a new user

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import {Validators, FormGroup, FormBuilder} from "@angular/forms";
+import {AuthService} from "../shared/security/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
 
-  constructor() { }
+  form:FormGroup;
+  
+  constructor(private fb:FormBuilder,
+              private authService: AuthService,
+              private router:Router) {
 
-  ngOnInit() {
+    this.form = this.fb.group({
+        email: ['',Validators.required],
+        password: ['',Validators.required],
+        confirm: ['',Validators.required]
+    });
+
   }
 
+  isPasswordMatch() {
+    const val = this.form.value;
+
+    return val && val.password && val.password == val.confirm;
+  }
+
+  signUp() {
+    const val = this.form.value;
+    
+    this.authService.signUp(val.email, val.password)
+      .subscribe(
+        () => {
+          alert('User sucessfully Created');
+          this.router.navigateByUrl('/home');
+        },
+        err => alert(err)
+      );
+
+  }
+ 
 }
