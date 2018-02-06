26c8f948eab7f799007f1d44bbcbdf21926faae2
JordLoop <JordLoop@gmail.com>
2018-02-06 14:44:11 -0800

    add user to database upon register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Validators, FormGroup, FormBuilder} from "@angular/forms";
+import { UserService } from '../services/user.service';
 import {AuthService} from "../shared/security/auth.service";
 import {Router} from "@angular/router";
 
@@ -9,20 +10,28 @@ import {Router} from "@angular/router";
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-
+  users;
   form:FormGroup;
   
   constructor(private fb:FormBuilder,
               private authService: AuthService,
-              private router:Router) {
+              private router:Router,
+              private userSvc: UserService) {
 
     this.form = this.fb.group({
         email: ['',Validators.required],
+        alias: ['',Validators.required],
         password: ['',Validators.required],
         confirm: ['',Validators.required]
     });
 
   }
+
+  ngOnInit() {
+  }
+
+
+  
 
   isPasswordMatch() {
     const val = this.form.value;
@@ -35,9 +44,12 @@ export class RegisterComponent {
     
     this.authService.signUp(val.email, val.password)
       .subscribe(
-        () => {
+        result => {
           alert('User sucessfully Created');
-          this.router.navigateByUrl('/home');
+          delete val.password;
+          delete val.confirm;
+          this.userSvc.createUser(val, result.uid)
+          this.router.navigateByUrl('/');
         },
         err => alert(err)
       );
